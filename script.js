5e79e63289a1c260e89f33ba3b66bbdc816549cf
NPC-MOSSHEAD <firegaming.8768@gmail.com>
2025-03-06 19:29:52 +0530

    npc-gaming-zone

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleDarkMode() {
+    document.body.classList.toggle('light-mode');
+}
+function toggleChat() {
+    let chatBox = document.getElementById("chat");
+    chatBox.classList.toggle("open");
+}
